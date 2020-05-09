c800e614c24f872853bd09db863ef8757b4be836
Kittennik <55207633+Kittennik@users.noreply.github.com>
2020-05-09 15:04:50 +0300

    Исправлено

--- src/e2e/test.js
+++ src/e2e/test.js
@@ -1,11 +1,24 @@
 import puppetteer from 'puppeteer';
+const { fork } = require('child_process');
 
 jest.setTimeout(30000);
 describe('Validation', () => {
   let browser = null;
   let page = null;
+  let server = null;
   const baseUrl = 'http://localhost:9000';
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', () => {
+        reject();
+      });
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
     browser = await puppetteer.launch({
       //headless: false,
       //slowMo: 100,
@@ -15,6 +28,7 @@ describe('Validation', () => {
   });
   afterAll(async () => {
     await browser.close();
+    server.kill();
   });
   test('Valid', async () => {
     await page.goto(baseUrl);
